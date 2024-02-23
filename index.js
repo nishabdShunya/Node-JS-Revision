ff53a31a4a02a08fb01a886db8928c3c25e38b6f
Nishant Choubisa <nishantchoubisa92@gmail.com>
2024-02-23 19:32:40 +0530

    Add: Different Endpoints | Different Response Types

--- index.js
+++ index.js
@@ -5,9 +5,31 @@ const PORT = 3000;
 const server = http.createServer();
 
 server.on("request", (req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ id: 1, name: "Issac Newton" }));
+  if (req.url === "/friends") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ id: 1, name: "Issac Newton" }));
+  } else if (req.url === "/messages") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.write(`<html>
+                <body>
+                  <ul>
+                    <li>Hello, Sir Issac.</li>
+                    <li>How are you?</li>
+                    <li>Thank you for discovering gravity.</li>
+                  </ul>
+                </body>
+              </html>`);
+    res.end();
+  } else if (req.url === "/message") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("It was nice talking with Sir Issac Newton.");
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
 server.listen(PORT, (err, res) => {
