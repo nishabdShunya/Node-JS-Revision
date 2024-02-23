5473fa63ae1d1a8822fc33385cc1330b762549d5
Nishant Choubisa <nishantchoubisa92@gmail.com>
2024-02-23 19:54:21 +0530

    Add: Parameterized URLs / Routes / Endpoints

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const PORT = 3000;
 
 const server = http.createServer();
 
+const friends = [
+  { id: 0, name: "Nikola Tesla" },
+  { id: 1, name: "Issac Newton" },
+  { id: 2, name: "Albert Einstein" },
+];
+
 server.on("request", (req, res) => {
-  if (req.url === "/friends") {
+  const items = req.url.split("/"); // /friends/1 => ["", "friends", "1"]
+  if (items[1] === "friends") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify({ id: 1, name: "Issac Newton" }));
-  } else if (req.url === "/messages") {
+    if (items.length === 3) {
+      const friendIndex = Number(items[2]);
+      res.end(JSON.stringify(friends[friendIndex]));
+    } else {
+      res.end(JSON.stringify(friends));
+    }
+  } else if (items[1] === "messages") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
     res.write(`<html>
@@ -22,7 +34,7 @@ server.on("request", (req, res) => {
                 </body>
               </html>`);
     res.end();
-  } else if (req.url === "/message") {
+  } else if (items[1] === "message") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.end("It was nice talking with Sir Issac Newton.");
